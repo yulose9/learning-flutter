aa95b2f54d1fbc5a847aa2e79956e38788d44c02
John Nazarene <jannazarene09@gmail.com>
2023-12-01 17:41:37 +0800

    Add answer button and quiz questions screens

---
+++ adv_basics/lib/answer_button.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  @override
+  Widget build(context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ adv_basics/lib/questions_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+
+  @override
+  State<QuestionScreen> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  @override
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
+  }
+}
