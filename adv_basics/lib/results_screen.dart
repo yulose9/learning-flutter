2b07e5274cd9a42d6938d22479052580e9596a52
John Nazarene <jannazarene09@gmail.com>
2023-12-17 11:26:53 +0800

    Learned how to use Firebase

--- adv_basics/lib/results_screen.dart
+++ adv_basics/lib/results_screen.dart
@@ -13,7 +13,7 @@ class ResultScreen extends StatelessWidget {
   final void Function() onRestart;
   final List<String> chosenAnswer;
 
-  List<Map<String, Object>> getSummaryData() {
+  List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summary = [];
 
     for (var i = 0; i < chosenAnswer.length; i++) {
@@ -32,11 +32,10 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final summaryData = getSummaryData();
     final numTotalQuestion = questions.length;
-    final numCorrectAnswer = summaryData.where((data) {
-      return data['user_answer'] == data['correct_answer'];
-    }).length;
+    final numCorrectAnswer = summaryData
+        .where((data) => data['user_answer'] == data['correct_answer'])
+        .length;
 
     return SizedBox(
       width: double.infinity,
